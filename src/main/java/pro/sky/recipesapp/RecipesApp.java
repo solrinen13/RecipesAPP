64a68da515838528dc95dffad9bee80f9c19e1bb
Saveliy <saveliy.kan@gmail.com>
2023-02-25 22:11:07 +0300

    1 commit

---
+++ src/main/java/pro/sky/recipesapp/RecipesApp.java
@@ -0,0 +1,13 @@
+package pro.sky.recipesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipesApp.class, args);
+    }
+
+}
